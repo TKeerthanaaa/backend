f0a2025a8e4469d35107cd8507af3d9b191d7681
Keerthana <keerthanathirumoorthy20>
2024-02-28 20:42:38 +0530

    backend

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
-// app.use("/", (req, res) => {
-//   res.status(200).send("<h1>Welcome to the product server</h1>");
-// });
+app.use("/", (req, res) => {
+  res.status(200).send("<h1>Welcome to the product server</h1>");
+});
 mongoose
   .connect(
     "mongodb+srv://Keerthana:Keerthana-0620@cluster0.w6ukcbk.mongodb.net/Blog?retryWrites=true&w=majority"
